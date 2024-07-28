3a78a2e447a205ddebc2666e17c0524753190fa7
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-07-28 19:58:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SideBar from "./components/SideBar";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <SideBar />
+    </main>
+  );
+}
+
+export default App;
